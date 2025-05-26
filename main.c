bbdf18d36fce9a59a6968026bb0512b0a8ad332a
gautam324 <147255351+gautam324@users.noreply.github.com>
2025-05-26 13:16:29 +0530

    improved ui

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
-void exchange(int *x, int *y) {
-  int temp = *x;
-  *x = *y;
-  *y = temp;
+int add(int *x, int *y) {
+  int z=*x+*y;
+  return z;
 }
 
 int main() {
   int m = 100;
-  int n = 200;
-  exchange(&m, &n);
-  return (m);  // After swap, m == 200
+  int n = 500;
+  int y=add(&m, &n);
+  return (y);  // output=600
 }
